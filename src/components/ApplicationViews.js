a89606604a697093a253bf21fc87f5a034c34af8
TrentSisson <trent.c.sisson0@gmail.com>
2021-03-04 09:59:25 -0600

    added animals provider and list and rendered to the dom

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+import { AnimalList } from "./animal/AnimalList"
+import { AnimalProvider } from "./animal/AnimalProvider"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route exact path="/animals">
+                <AnimalProvider>
+                    <AnimalList />
+                </AnimalProvider>
+            </Route>
+            {/* <Route path="/locations">
+                <LocationProvider>
+                    <LocationList />
+                </LocationProvider>
+            </Route>
+            <Route path="/customers">
+                <CustomerProvider>
+                    <CustomerList />
+                </CustomerProvider>
+            </Route>
+            <Route path="/employees">
+                <EmployeeProvider>
+                    <EmploeeList />
+                </EmployeeProvider>
+            </Route> */}
+        </>
+    )
+}
